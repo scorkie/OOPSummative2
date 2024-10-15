159a2aad5e550430ae301b2f23d3fec529fa2d44
Keian Mar <150799485+7kei@users.noreply.github.com>
2024-10-15 20:46:09 +0800

    Move Rescue Items into its own class, programatically add items

--- OOPSummative2/firstAid.Designer.cs
+++ OOPSummative2/firstAid.Designer.cs
@@ -32,6 +32,7 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,20 +41,31 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Image = global::OOPSummative2.Properties.Resources.backgroudn;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(1793, 896);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(246, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ADD BUTTON NI SHA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // firstAid
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1345, 728);
+            this.ClientSize = new System.Drawing.Size(1793, 896);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "firstAid";
             this.Text = "firstAid";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -64,5 +76,6 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- OOPSummative2/firstAid.cs
+++ OOPSummative2/firstAid.cs
@@ -12,140 +12,23 @@ namespace OOPSummative2
 {
     public partial class firstAid : Form
     {
+        int buttonPresses = 0;
+
         public firstAid()
         {
             InitializeComponent();
 
-            int baseX = 100;
-            int baseY = 14;
-
-            itemToPanel(baseX, baseY);
-
-            for (int i = 0; i < 3; i++)
-            {
-                int locX = baseX + 271 * (i + 1);
-                int locY = 14;
-                itemToPanel(locX, locY);
-            }
-            
         }
 
-        private void itemToPanel(int x, int y)
+        private void button1_Click(object sender, EventArgs e)
         {
-            Panel background = new Panel();
-            PictureBox itemPictureBox = new PictureBox();
-            Label priceLabel = new Label();
-            Label titleLabel = new Label();
-            TextBox itemCount = new TextBox();
-            PictureBox itemPlus = new PictureBox();
-            PictureBox itemMinus = new PictureBox();
-            ((ISupportInitialize)(itemMinus)).BeginInit();
-            ((ISupportInitialize)(itemPlus)).BeginInit();
-            ((ISupportInitialize)(itemPictureBox)).BeginInit();
+            buttonPresses++;
 
-            // 
-            // background
-            // 
-            background.BackColor = Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(216)))), ((int)(((byte)(216)))));
-            background.Controls.Add(itemMinus);
-            background.Controls.Add(itemPlus);
-            background.Controls.Add(itemCount);
-            background.Controls.Add(priceLabel);
-            background.Controls.Add(titleLabel);
-            background.Controls.Add(itemPictureBox);
-            background.Location = new Point(x, y);
-            background.Margin = new Padding(4, 4, 4, 4);
-            background.Name = "background";
-            background.Size = new System.Drawing.Size(240, 283);
-            background.TabIndex = 3;
-            // 
-            // itemMinus
-            // 
-            itemMinus.Image = global::OOPSummative2.Properties.Resources._2;
-            itemMinus.Location = new Point(139, 246);
-            itemMinus.Margin = new Padding(4, 4, 4, 4);
-            itemMinus.Name = "itemMinus";
-            itemMinus.Size = new Size(21, 23);
-            itemMinus.SizeMode = PictureBoxSizeMode.CenterImage;
-            itemMinus.TabIndex = 5;
-            itemMinus.TabStop = false;
-            itemMinus.MouseClick += new MouseEventHandler(bndgMinus_MouseClick);
-            // 
-            // itemPlus
-            // 
-            itemPlus.Image = global::OOPSummative2.Properties.Resources._13;
-            itemPlus.Location = new Point(197, 246);
-            itemPlus.Margin = new Padding(4, 4, 4, 4);
-            itemPlus.Name = "itemPlus";
-            itemPlus.Size = new Size(21, 23);
-            itemPlus.SizeMode = PictureBoxSizeMode.CenterImage;
-            itemPlus.TabIndex = 4;
-            itemPlus.TabStop = false;
-            itemPlus.MouseClick += new MouseEventHandler(bndgPlus_MouseClick);
-            // 
-            // itemCount
-            // 
-            itemCount.Location = new Point(164, 245);
-            itemCount.Margin = new Padding(4, 4, 4, 4);
-            itemCount.Name = "itemCount";
-            itemCount.Size = new Size(29, 22);
-            itemCount.TabIndex = 3;
-            itemCount.Text = "0";
-            itemCount.TextAlign = HorizontalAlignment.Center;
-            // 
-            // priceLabel
-            // 
-            priceLabel.AutoSize = true;
-            priceLabel.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0)));
-            priceLabel.Location = new Point(17, 240);
-            priceLabel.Margin = new Padding(4, 0, 4, 0);
-            priceLabel.Name = "priceLabel";
-            priceLabel.Size = new Size(42, 18);
-            priceLabel.TabIndex = 2;
-            priceLabel.Text = "₱225";
-            // 
-            // titleLabel
-            // 
-            titleLabel.AutoSize = true;
-            titleLabel.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(0)));
-            titleLabel.Location = new Point(15, 218);
-            titleLabel.Margin = new Padding(4, 0, 4, 0);
-            titleLabel.Name = "titleLabel";
-            titleLabel.Size = new Size(122, 18);
-            titleLabel.TabIndex = 1;
-            titleLabel.Text = "Bandage (5 pcs)";
-
-            // 
-            // itemPictureBox
-            // 
-            itemPictureBox.Image = global::OOPSummative2.Properties.Resources.bandage;
-            itemPictureBox.Location = new Point(17, 16);
-            itemPictureBox.Margin = new Padding(4, 4, 4, 4);
-            itemPictureBox.Name = "itemPictureBox";
-            itemPictureBox.Size = new Size(205, 197);
-            itemPictureBox.TabIndex = 0;
-            itemPictureBox.TabStop = false;
-
-
-            ((ISupportInitialize)(itemMinus)).EndInit();
-            ((ISupportInitialize)(itemPlus)).EndInit();
-            ((ISupportInitialize)(itemPictureBox)).EndInit();
-
-            itemPictureBox.BringToFront();
-            background.BringToFront();
-
-            Controls.Add(background);
+            int locX = 100 * (buttonPresses + 1);
+            int locY = 14 + (296 * (buttonPresses / 5));
+            RescueItem item = new RescueItem();
+            item.itemToPanel(this, locX, locY);
             pictureBox1.SendToBack();
-        }
-
-        private void bndgPlus_MouseClick(object sender, MouseEventArgs e)
-        {
-            MessageBox.Show("Plus 1");
-        }
-
-        private void bndgMinus_MouseClick(object sender, MouseEventArgs e)
-        {
-            MessageBox.Show("Minus 1");
         }
     }
 }
